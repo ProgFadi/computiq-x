96d99e26f08ce81dba968d42a35d25b38d7670b6
Abbas Salah <abbas.log01@gmail.com>
2021-12-16 14:39:10 +0300

    Done

--- src/views/BoardView.jsx
+++ src/views/BoardView.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState } from 'react';
+import styles from "../App.module.css";
+import Table from "../components/Table";
+function BoardView() {
+    const [pointsList, setpointsList] = useState(([]))
+    useEffect(()=>{
 
-function BoardView(props) {
+        let webApiUrl =  'https://website-backend.computiq.tech/api/score/data/5';
+        let tokenStr = JSON.parse(localStorage.getItem('token'));
+        axios.get(webApiUrl, { headers: {"Authorization" : `${tokenStr.token.token_type} ${tokenStr.token.access_token}`} }).then((response)=>{
+            setpointsList(response.data.data)
+        })
+    },[])
     return (
-        <div>
-            BoardView
+        <main className={styles.container}>
+        <div className={styles.wrapper}>
+          <Table data={pointsList} rowsPerPage={10} />
         </div>
+      </main>
     );
 }
 
