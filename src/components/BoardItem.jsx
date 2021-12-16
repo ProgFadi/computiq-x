d7181dc825d2acea616e92f5db6e4c1f2841213a
Karrar <karrarprogrammer01@gmail.com>
2021-12-16 22:09:32 +0300

    solve

---
+++ src/components/BoardItem.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Box,makeStyles, Typography } from '@material-ui/core';
+
+
+const useStyles = makeStyles(()=>({
+    flx:{
+        color: '#575756',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems:'center'
+    },
+
+    box: {
+        display: 'flex',
+        alignItems:'center'
+    },
+
+    img: {
+        fontSize: 35,
+        marginLeft: 5
+    }
+
+}))
+
+
+export default function BoardItem(props) {
+    const classes = useStyles()
+    return (
+        <>
+        <Typography variant="h4" component="div">SCORE BOARD</Typography>
+        {
+            props.dataP.map((item,i)=>{
+                return (
+                    <Box className={classes.flx} key={i}>
+                        <Box className={classes.box}>
+                            <Typography variant="h6" component="div">#{props.data.findIndex((data)=> (data.user__first_name+' '+data.user__last_name)===(item.user__first_name+' '+item.user__last_name) )}</Typography>
+                            <Typography variant="h6" component="div" className={classes.img}>{item.user__score_profile__photo}</Typography>
+                        </Box>
+                        <Typography variant="h6" component="div">{item.user__first_name+' '+item.user__last_name}</Typography>
+                        <Typography variant="h6" component="div">{item.total_score} points</Typography>
+                    </Box>
+                )
+            })
+        }    
+        </>
+    )
+}
--- src/views/BoardView.jsx
+++ src/views/BoardView.jsx
@@ -1,18 +1,60 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
+import { Box , makeStyles} from '@material-ui/core';
+import React, { useEffect, useState} from 'react';
 import { DashboradService } from '../services/Http/DashoardService';
+import Pagination from '@mui/material/Pagination';
+import BoardItem from '../components/BoardItem';
 
+const useStyles = makeStyles((theme)=>({
+    bg:{
+        margin: '10px auto',
+        borderRadius: 20,
+        boxSizing: 'border-box',
+        padding: 20,
+        border: '1px #1a8cf7 solid',
+        boxShadow: '5px 5px 0 #1a8cf7',
+        [theme.breakpoints.down('md')]: {
+            width: '90%',
+          },
+        [theme.breakpoints.up('md')]: {
+            width: '50%',
+          },
+    },
+
+
+}))
 function BoardView(props) {
-    // data state/ points
-    // loading from 
-    // 
+    const classes = useStyles()
+    let perPage = 10
+    const [data, setData] = useState([])
+    const [page, setPage] = useState(1)
+    const [pages, setPages] = useState(1)
+    const [dataPaginate, setDataPaginate] = useState([])
+
+    const handleClick = (event, value)=> {
+        let fromRecord = perPage * (value - 1)
+        let toRecord = ((fromRecord + perPage) <= data.length) ? (fromRecord + perPage) : (data.length % perPage) + fromRecord
+        setPage(value)
+        setDataPaginate(data.slice(fromRecord,toRecord))
+    }
+
+    
     useEffect(()=>{
     
-      
-
+        (new DashboradService).loadPoints().then((res)=> {
+            let d = res.data.data
+            setData([...d])
+            setPages(Math.ceil(d.length/perPage))
+            setDataPaginate(d.slice(0,10))
+        }).catch((err)=> console.log(err))
+        
     },[])
+
     return (
         <div>
+            <Box className={classes.bg}>
+                <BoardItem dataP={dataPaginate} data={data} />
+                <Pagination color="primary" count={pages} page={page} onChange={handleClick} />
+            </Box>
            
         </div>
     );
