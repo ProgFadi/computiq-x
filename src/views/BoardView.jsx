d96fdd6d401e664127eadfc372b2126cb1f24001
ProgFadi <fadiramzi.99@gmail.com>
2021-12-16 22:15:35 +0300

    fix issue of switching theme by unifying some pakages

--- src/views/BoardView.jsx
+++ src/views/BoardView.jsx
@@ -2,15 +2,18 @@ import axios from 'axios';
 import React, { useEffect } from 'react';
 import { DashboradService } from '../services/Http/DashoardService';
 
+
 function BoardView(props) {
     // data state/ points
     // loading from 
     // 
+    // 2
     useEffect(()=>{
     
-      
+      // update on point array
 
     },[])
+    // 1
     return (
         <div>
            
