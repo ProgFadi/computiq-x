d2d983c4219b7875254991ee43aee293cb068eb6
abdullahamer <abbnntt5565@gmail.com>
2021-12-18 21:09:13 +0300

    abdullah amer

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,25 +1,33 @@
-import React from 'react';
-
-const Pagination = ({ DatasPerPage, totalDatas, paginate }) => {
-  const pageNumbers = [];
-
-  for (let i = 1; i <= Math.ceil(totalDatas / DatasPerPage); i++) {
-    pageNumbers.push(i);
-  }
-
-  return (
-    <nav>
-      <ul >
-        {pageNumbers.map(number => (
-          <li key={number} >
-            <a onClick={() => paginate(number)} href='!#'>
-              {number}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-};
-
-export default Pagination;
+// import * as React from 'react';
+// import { Link, MemoryRouter, Route, Routes, useLocation } from 'react-router-dom';
+// import Pagination from '@mui/material/Pagination';
+// import PaginationItem from '@mui/material/PaginationItem';
+//
+// function Content() {
+//     const location = useLocation();
+//     const query = new URLSearchParams(location.search);
+//     const page = parseInt(query.get('page') || '1', 10);
+//     return (
+//         <Pagination
+//             page={page}
+//             count={10}
+//             renderItem={(item) => (
+//                 <PaginationItem
+//                     component={Link}
+//                     to={`/inbox${item.page === 1 ? '' : `?page=${item.page}`}`}
+//                     {...item}
+//                 />
+//             )}
+//         />
+//     );
+// }
+//
+// export default function PaginationLink() {
+//     return (
+//         <MemoryRouter initialEntries={['/inbox']} initialIndex={0}>
+//             <Routes>
+//                 <Route path="*" element={<Content />} />
+//             </Routes>
+//         </MemoryRouter>
+//     );
+// }
--- src/views/BoardView.jsx
+++ src/views/BoardView.jsx
@@ -4,14 +4,16 @@ import styled from "styled-components";
 import axiosInstance from "../utils/axios";
 import {SESSION_KEY} from "../common/Constants";
 import axios from "axios";
-import Pagination from '../components/Pagination';
+// import Pagination from '../components/Pagination';
 
 const Bodyd=styled.div`
   background-color:white;
   margin-left: 600px;
-  box-shadow: blue;
-  -o-box-shadow: darkblue;
-  height: 550px;
+  -webkit-box-shadow: 3px 3px 5px 6px blue;  /* Safari 3-4, iOS 4.0.2 - 4.2, Android 2.3+ */
+  -moz-box-shadow:    3px 3px 5px 6px blue;  /* Firefox 3.5 - 3.6 */
+  box-shadow:         3px 3px 5px 6px blue;
+  //box-shadow: inset 0 0 10px blue;
+
   border-radius: 30px;
   //margin-right: 1500px;
   margin-top: 50px;
@@ -31,50 +33,26 @@ const Rowdiv=styled.div`
 
 function BoardView(props) {
     const [dashboard, setdashboard] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [datasPerPage] = useState(7);
+
     // const apiToken =JSON.parse(localStorage.getItem(SESSION_KEY));
     const token='Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjI3YjgyZjZjLTgwNmUtNGUwMy04NWRkLTU5NjVmZTc2MWY0ZiIsImV4cCI6MTY0NDc3OTI5OSwic3ViIjoiYWNjZXNzIn0.t8g0AmABZX15lcBvCKa5y3Cjc97rVPCX9CXEy2z2Kac';
 
-    // useEffect(()=>{
-    //     (new DashboardService).loadDashboard(token).then(
-    //         (res)=>{
-    //             res=res.data;
-    //             setboard(res)
-    //         }
-    //     ).catch(
-    //         console.log('error')
-    //     )
-    //     },[]
-    // )
     useEffect(()=>{
         axios.get('https://website-backend.computiq.tech/api/score/data/5' ,
             { headers: {"Authorization" : token}
             }).then(
             (res)=>{
-                setLoading(true);
+
                 const req=res.data.data;
                 console.log(res.data)
                 setdashboard(req);
-                setLoading(false);
+
 
             }
         )
     })
-    //program__title: "Full-Stack Development Bootcamp"
-    // total_score: "64"
-    // user__first_name: "Karrar"
-    // user__last_name: "Emad Ali"
-    // user__score_profile__photo: "🖱"
 
-    // Get current datas
-    const indexOfLastDatas = currentPage * datasPerPage;
-    const indexOfFirstData = indexOfLastDatas - datasPerPage;
-    const currentDatas = dashboard.slice(indexOfFirstData, indexOfLastDatas);
 
-    // Change page
-    const paginate = pageNumber => setCurrentPage(pageNumber);
 
     return (
         <div style={{background:'$F5F5F5'}}>
@@ -93,12 +71,6 @@ function BoardView(props) {
                     }
                     
 
-                {/*<Datas datas={currentDatas} loading={loading} />*/}
-                <Pagination
-                    datasPerPage={datasPerPage}
-                    totaldatas={dashboard.length}
-                    paginate={paginate}
-                />
             </Bodyd>
         </div>
     );
