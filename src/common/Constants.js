476af9c8fdfa429de701ce325aee0fef4c371ff4
Zahraa Basheer <68440929+zhr9797@users.noreply.github.com>
2021-12-22 15:10:23 +0300

    Delete src directory

--- src/common/Constants.js
+++
@@ -1,8 +0,0 @@
-export const SESSION_KEY = "CompUserData";
-export const BASE_URL = "https://website-backend.computiq.tech";
-
-export const THEMES = {
-    LIGHT: 'LIGHT',
-    ONE_DARK: 'ONE_DARK'
-  };
-  
